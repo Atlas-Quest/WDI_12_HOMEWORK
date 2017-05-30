9252fe475b067a19a2ab2b1eaf1521020c9fb396
Vanessa Dohnt <vanessa.e.dohnt@gmail.com>
2017-05-31 09:52:54 +1000

    loops homework

---
+++ Vanessa/wk01/2-tue/loops.js
@@ -0,0 +1,21 @@
+// The Odd/Even Reporter
+
+var count = 1
+
+for (count = 1; count <=20; count++){
+
+  if (count % 2 === 0)
+  console.log(count + ' ' + 'is even');
+  else
+  console.log(count + ' ' + 'is odd');
+}
+
+// Multiplication Tables
+
+var count = 1
+
+for (count = 1; count <=10; count++){
+  console.log(count + ' x 9 = ' + count * 9);
+}
+
+// Your top choices - Incomplete
