531f00de51e814595ba058e448b8fc90c1d22daf
Vanessa.Dohnt <vanessa.e.dohnt@gmail.com>
2017-06-02 10:29:32 +1000

    missing homework

---
+++ Vanessa/wk01/3-wed/arrays.js
@@ -0,0 +1,6 @@
+var days_of_the_week = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']; {
+  days_of_the_week.splice(0,0,'Sun');
+  days_of_the_week.pop();
+  console.log(days_of_the_week);
+}
+var days_of_the_week = [['Mon','Tue','Wed','Thu','Fri'],['Sat','Sun']];
