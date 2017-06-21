b1bb0077c05350243c409a0ccef5778950d2eb92
Vanessa Dohnt <vanessadohnt@Vanessas-MacBook-Pro.local>
2017-06-22 09:06:05 +1000

    monkey homework

---
+++ Vanessa/wk04/4-thu/monkey.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+puts "What is your monkey's name? "
+name = gets.chomp.to_s
+
+puts "What is your monkey's species? "
+species = gets.chomp.to_s
+
+puts "What has your Monkey eaten today? "
+foods = gets.chomp.to_s
+foods_eaten = []
+
+binding pry
