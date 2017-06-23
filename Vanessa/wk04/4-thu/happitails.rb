175380ce561495724bd4ae591050a26a1eec457c
Vanessa Dohnt <vanessadohnt@Vanessas-MacBook-Pro.local>
2017-06-23 11:33:10 +1000

    test

---
+++ Vanessa/wk04/4-thu/happitails.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class animals
+  @name
+  def initialize
+    @age
+    @species
+    @breed
+  end
+end
+
+class clients
+  def initialize
+    @address
+    @contact_number
+    
+  end
+end
+
+binding.pry
---
+++ Vanessa/wk04/5-fri/test.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class = game
+
+  def initialize
+    @card
+  end
+
+  def ritual
+    puts "Pokemon! I choose you!"
+  end
+
+  def getCard
+    @card.sample
+  end
+end
+
+card1 = Pikachu.new(40)
+card2 = Rattata.new(20)
+card3 = Pidgeot.new(60)
+card4 = Alakazam.new(80)
+card5 = Butterfree.new(30)
+card6 = Gengar.new(70)
+card7 = Moltres.new(100)
+card8 = Vulpix.new(40)
+card9 = Blastoise.new(80)
+card10 = Hitmonchan.new(50)
