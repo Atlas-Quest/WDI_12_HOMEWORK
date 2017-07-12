c85f3f22c7ed653ca036155a4daabb01225d9d9b
Vanessa Dohnt <vanessadohnt@Vanessas-MacBook-Pro.local>
2017-07-12 21:43:55 +1000

    initial commit

---
+++ Vanessa/wk07/2-tue/ajax_movies/app.js
@@ -0,0 +1,36 @@
+var $btn = $('button');
+
+$btn.on('click', function(event) {
+
+  event.preventDefault();
+
+  var movieName = document.querySelector('input').value;
+
+  var inputBox = document.querySelector('input').value;
+
+  var settings = {
+    url: 'http://omdbapi.com',
+    data: {
+      t: movieName,
+      apikey: '2f6435d9'
+  }
+}
+
+jQuery.ajax(settings).done(function(response) {
+  var newItem = document.createElement('p');
+  newItem.textContent = response.Title
+  document.querySelector('.response').append(newItem);
+})
+console.log('after ajax function call');
+});
+
+
+// var searchMovies = function() {
+//   var query = document.querySelector("input").value;
+//   var movieList = [];
+//   $.ajax(settings).done(function(movies){
+//     $.each(movies.Search,function(ind,movie){
+//       movieList.push(movie.Title);
+//     })
+//   });
+// }
