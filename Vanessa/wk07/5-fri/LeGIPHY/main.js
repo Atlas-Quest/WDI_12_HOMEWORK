9f1fe5c3e4db2545e1329c809dd557416ea45752
Vanessa Dohnt <vanessadohnt@Vanessas-MacBook-Pro.local>
2017-08-08 22:50:25 +1000

    completed le giphy

--- Vanessa/wk07/5-fri/LeGIPHY/main.js
+++ Vanessa/wk07/5-fri/LeGIPHY/main.js
@@ -1,25 +1,34 @@
 search =  $('form').submit(function(event){
   event.preventDefault();
-  var giphs = $('input').val();
+
+  var giph = $('input').val();
 
   var settings = {
 
     url: 'http://api.giphy.com/v1/gifs/search',
     data: {
-      q: giphs,
-      apiKey: '8792c364a89a488f96989e3fd221dcf6',
-      limit: 10
+      q: giph,
+      api_key: '8792c364a89a488f96989e3fd221dcf6',
+      limit: 10,
+      offset: 0
     }
   }
 
-  console.log(giphs);
-
   $.ajax(settings).done(displaySearches)
+
+  $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() > $(document).height()) {
+      $('.results').append($.ajax(request).done(displaySearches))
+    }
+  })
 })
 
-function displaySearches(response){
-  image = response['data'][0].images.fixed_height
-  for (var i = 0; i < image.length; i++) {
-    image[i].each
-  }
+
+function displaySearches(response) {
+  console.log(response);
+
+  response.data.forEach(function(picture){
+    var image = $('<img>').attr('src', picture.images.original.url)
+    $('.results').append(image)
+  })
 }
