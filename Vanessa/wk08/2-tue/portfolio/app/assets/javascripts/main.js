1e9595d417b7df282b92b2b69e55cd2899ba7033
Vanessa Dohnt <vanessadohnt@Vanessas-MacBook-Pro.local>
2017-09-12 18:24:36 +1000

    updated

--- Vanessa/wk08/2-tue/portfolio/app/assets/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-$("a.toscroll").on('click',function(e) {
-    var url = e.target.href;
-    var hash = url.substring(url.indexOf("#")+1);
-    $('html, body').animate({
-        scrollTop: $('#'+hash).offset().top
-    }, 500);
-    return false;
-});
